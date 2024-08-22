0689bdb9ebe4f1bee28fe3c18ec08377bf540bf6
Caio Fernando <caiofsr@proton.me>
2024-08-22 17:13:45 -0300

    change to lazy git

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,12 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+}
